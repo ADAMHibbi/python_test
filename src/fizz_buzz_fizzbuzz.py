4219d1dbeb16fcc85108f11055e09e9f4b6ef45e
Adem Hibbi <ademhibbi@gmail.com>
2025-10-22 22:59:43 +0100

    fizz with 6 I forget number6 bizz with 5 and 10

--- src/fizz_buzz_fizzbuzz.py
+++ src/fizz_buzz_fizzbuzz.py
@@ -1,4 +1,7 @@
 def fizz_buzz_fizzbuzz(n):
     if n % 3 == 0:
         return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
     return n
+
--- test/test_fizz_buzz_fizzbuzz.py
+++ test/test_fizz_buzz_fizzbuzz.py
@@ -1,6 +1,18 @@
-from src.fizz_buzz_fizzbuzz import fizzbuzz
+from src.fizzbuzz import fizzbuzz
+
 def fizz_3():
     assert fizzbuzz(3) == "Fizz"
 
+def fizz_6():
+    assert fizzbuzz(6) == "Fizz"
+
+def buzz_5():
+    assert fizzbuzz(5) == "Buzz"
+
+def buzz_10():
+    assert fizzbuzz(10) == "Buzz"
+
 def fizz_1():
     assert fizzbuzz(1) == 1
+
+
