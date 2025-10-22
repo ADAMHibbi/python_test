a62ba3732847d0074c860301c805d6414eef68c5
Adem Hibbi <ademhibbi@gmail.com>
2025-10-22 22:38:31 +0100

    fizz with 3 and 1

---
+++ src/fizz_buzz_fizzbuzz.py
@@ -0,0 +1,4 @@
+def fizz_buzz_fizzbuzz(n):
+    if n % 3 == 0:
+        return "Fizz"
+    return n
---
+++ test/test_fizz_buzz_fizzbuzz.py
@@ -0,0 +1,6 @@
+from src.fizz_buzz_fizzbuzz import fizzbuzz
+def fizz_3():
+    assert fizzbuzz(3) == "Fizz"
+
+def fizz_1():
+    assert fizzbuzz(1) == 1
